75de59d6dceb2d203be0f898dc58b4db0489a2da
samer71 <samer_aoudi@hotmail.com>
2013-10-12 23:03:58 +0400

    c

--- js/myApp.js
+++ js/myApp.js
@@ -8,16 +8,10 @@ $(document).ready(function() {
 var map, lat, lon, latlon, mylocation;
 var proxm, proxkm;
 var totalstores, storetype, storemarker;
-var zoomlevel, bounds;
+var zoomlevel, bounds, distance;
 var museumsArray;
 
 function onDeviceReady() {
-	$.getJSON('museums.json', function(json) {
-		var museumsArray=[];
-		$.each(json,function(index,value){ 
-			$("#errorholder").append(index+": "+value.name);
-		});
-	});
 }
 
 function loadScript(zl,pm) {
@@ -47,12 +41,10 @@ $('#museumspage').click(function() {
 });
 
 // This updates the map, listing and store page for every result
-function renderStore(store, label) {
-	var storelat=store.get('location').latitude;
-	var storelon=store.get('location').longitude;
-	var coords = storelat+","+storelon;
-	storelatlon=new google.maps.LatLng(storelat, storelon);
-	var distance = (google.maps.geometry.spherical.computeDistanceBetween (storelatlon, latlon)/1000).toFixed(1);
+function renderStore(myloc, prox,label,name,stlat,stlon) {
+	var coords = stlat+","+stlon;
+	var storelatlon=new google.maps.LatLng(stlat, stlon);
+	distance = (google.maps.geometry.spherical.computeDistanceBetween (storelatlon, latlon)/1000).toFixed(1);
 	// Extend the map to fit 
 	bounds.extend(storelatlon);
 	map.fitBounds(bounds);
@@ -64,12 +56,10 @@ function renderStore(store, label) {
 		map:map});
 		
 	// Append to the list of results
-	$("#list").append('<li class="onestore"><a href="#page'+store.get('museumNumber')+'" data-role="button" data-transition="slide">'+store.get('name')+' ('+distance+'KM)</a><span class="ui-li-count ui-btn-corner-all">'+label+'</span></li>');
+	$("#list").append('<li class="onestore"><a href="#page'+label+'" data-role="button" data-transition="slide">'+name+' ('+distance+'KM)</a><span class="ui-li-count ui-btn-corner-all">'+label+'</span></li>');
 
 	// Necessary for the listview to render correctly
 	$("#list").listview('refresh');
-	
-	$('body').append('<div data-role="page" id="page'+store.get('museumNumber')+'"><div data-theme="b" data-role="header" data-position="fixed"><h3>'+store.get('name')+'</h3><a data-role="button" data-rel="back" data-icon="arrow-l" data-iconpos="left"class="ui-btn-left">Results</a></div><img id="map" src="https://maps.googleapis.com/maps/api/staticmap?scale=2&center='+coords+'+&zoom=11&size='+window.innerWidth+'x200&markers=color:yellow%7Clabel:'+label+'%7C'+coords+'&markers=color:red%7Clabel:M%7C'+latlon+'&path=color:0x0000ff%7Cweight:5%7C'+coords+'%7C'+latlon+'&sensor=false" height="200"/><div data-role="content"><p><b>Address('+distance+'KM from you)</b><br/>'+store.get('location').displayAddress+'</p>'+store.get('description')+'<p><b>Entry Fees</b><br/>'+store.get('entryFees').join('<br/>')+'</p>'+'<p><b>Opening Hours</b><br/>'+store.get('hours').join('<br/>')+'</p>'+'<p><b>Contacts</b><br/>'+store.get('contact').join('<br/>')+'</p></div></div>');
 }
 
 
@@ -105,24 +95,14 @@ function onGetLocationSuccess(position)
   
 function getStores(ml,pm,st)
 {
-	  // Initialize a local “stores” object using the SDK
-      var stores = new Usergrid.Collection(st);
-	  var lim=30;
-	  // This query will return all stores within X meters of the current location
-      stores.setQueryParams({"ql":"select * where location within "+pm+" of "+ml,"limit":"100"});
-	 // Do the actual query and process the results
-	stores.fetch( function(response) {
-		// Process the regular store
-		var count = 1;
-		while(stores.hasNextEntity()) {
-			renderStore(stores.getNextEntity(), count);
-			//localStorage.setItem('museumObject', JSON.stringify(stores));
-			count++;
-		} // End while
-		//var retrievedObject = localStorage.getItem('museumObject');
-		//$("#errorholder").text(JSON.parse(retrievedObject));
+		var totalstores = 0;
+		$.getJSON('museums.json', function(json) {
+			$.each(json,function(index,value){ 
+				renderStore(ml,pm, index,value.name, value.location.latitude, value.location.longitude);
+				totalstores++;
+			});
+		});
 		// Remove the loader
-		totalstores=count-1;
 		$("#totalstores").html(totalstores);
 		if(totalstores==0)
 		{
@@ -133,7 +113,6 @@ function getStores(ml,pm,st)
 		{
 			$("#totalstores").html(totalstores);
 		}
-	}); // end fetch store
 }
 
 function onGetLocationError(error)
