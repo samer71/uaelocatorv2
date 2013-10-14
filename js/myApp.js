226cd526f2eb8f812d68b186a8f7040b5a92077d
samer71 <samer_aoudi@hotmail.com>
2013-10-14 15:19:56 +0400

    d

--- js/myApp.js
+++ js/myApp.js
@@ -52,14 +52,12 @@ function renderStore(prox,label,name,stlat,stlon,da,ef,h,c,desc) {
 			position:storelatlon,
 			map:map});
 		// Append to the list of results
-		$("#list").append('<li class="onestore"><a href="#page'+label+'" data-role="button" data-transition="slide">'+name+' ('+distance+'KM)</a><span class="ui-li-count ui-btn-corner-all">'+label+'</span></li>');
+		$("#list").append('<li class="onestore" id="'+label+'"><a href="#details" data-role="button" data-transition="slide">'+name+' ('+distance+'KM)</a><span class="ui-li-count ui-btn-corner-all">'+label+'</span></li>');
 	} // End if
 	// Necessary for the listview to render correctly
 	$("#list").listview('refresh');
 	$("#totalstores").html(totalstores);
-	
-	$('body').append('<div data-role="page" id="page'+label+'"><div data-theme="b" data-role="header" data-position="fixed"><h3>'+name+'</h3><a data-role="button" data-rel="back" data-icon="arrow-l" data-iconpos="left">Results</a></div><img id="map" src="https://maps.googleapis.com/maps/api/staticmap?scale=2&center='+coords+'+&zoom=11&size='+window.innerWidth+'x200&markers=color:yellow%7Clabel:'+label+'%7C'+coords+'&markers=color:red%7Clabel:M%7C'+latlon+'&path=color:0x0000ff%7Cweight:5%7C'+coords+'%7C'+latlon+'&sensor=false" height="200"/><div data-role="content"><p><b>Address('+distance+'KM from you)</b><br/>'+da+'</p>'+desc+'<p><b>Entry Fees</b><br/>'+ef.join('<br/>')+'</p>'+'<p><b>Opening Hours</b><br/>'+h.join('<br/>')+'</p>'+'<p><b>Contacts</b><br/>'+c.join('<br/>')+'</p></div></div>');
-	
+		
 } // End renderStores Function
 
 function updateAll()
@@ -198,7 +196,10 @@ $('.beachespage').click(function() {
 	loadScript(11,10000);
 });
 
-
+$('.onestore').click(function(event) {
+	storetype="beaches";
+	$("#storename").html(event.target.id);
+});
 
 $('#reload').click(function() {
 	loadScript(12,10000);
