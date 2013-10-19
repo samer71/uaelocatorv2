bed625cc1dbab23d5968299ea9432608e2e7122d
samer71 <samer_aoudi@hotmail.com>
2013-10-19 13:45:52 +0400

    f

--- js/myApp.js
+++ js/myApp.js
@@ -337,3 +337,29 @@ $('#options').delegate('.option', 'tap', function ()  {
 		//$.mobile.showPageLoadingMsg("e", "Locating...");
 	} // End else network
 });
+
+function downloadFile(){
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
+    function onFileSystemSuccess(fileSystem) {
+        fileSystem.root.getFile(
+        "dummy.html", {create: true, exclusive: false}, 
+        function gotFileEntry(fileEntry) {
+            var sPath = fileEntry.fullPath.replace("dummy.html","");
+            var fileTransfer = new FileTransfer();
+            fileEntry.remove();
+            fileTransfer.download(
+                "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf",
+                sPath + "theFile.pdf",
+                function(theFile) {
+                    alert("download complete: " + theFile.toURI());
+                    showLink(theFile.toURI());
+                },
+                function(error) {
+                    alert("download error source " + error.source);
+                    alert("download error target " + error.target);
+                    alert("upload error code: " + error.code);
+                }
+            );
+        }, fail);
+    }, fail);
+}
