4805d6683d9c6b2ef8169f7893a4aa6f24b73aa7
samer71 <samer_aoudi@hotmail.com>
2013-10-20 10:27:08 +0400

    d

--- js/myApp.js
+++ js/myApp.js
@@ -300,18 +300,24 @@ $('#list').delegate('.onestore', 'tap', function ()  {
 			$("#storefacilities").html(value.facilities);
 			$("#storefees").html(value.entryFees.join('<br/>'));
 			$("#storehours").html(value.hours.join('<br/>'));
-			if(value.contact.Tel!="")
-			$("#storephone").html('<a href="tel:'+value.contact.Tel+'">'+value.contact.Tel+'</a>');
-			else
-			$("#storephone").html('NA');
-			if(value.contact.Email!="")
-			$("#storeemail").html('<a href="mailto:'+value.contact.Email+'">'+value.contact.Email+'</a>');
-			else
-			$("#storeemail").html('NA');
-			if(value.contact.Website!="")
-			$("#storeweb").html('<a class="storeweblink" href="'+value.contact.Website+'">'+value.contact.Website+'</a>');
-			else
-			$("#storeweb").html('');
+			if(value.contact.Tel!="") {
+				$('.phonelink').attr('href')="+value.contact.Tel+";
+				$('.phonelink').html('+value.contact.Tel+')
+				//$("#storephone").html('<a href="tel:'+value.contact.Tel+'">'+value.contact.Tel+'</a>');
+			}
+			else $("#storephone").html('NA');
+			if(value.contact.Email!="") {
+				$('.emaillink').attr('href')="+value.contact.Email+";
+				$('.emaillink').html('+value.contact.Email+')
+				//$("#storeemail").html('<a href="mailto:'+value.contact.Email+'">'+value.contact.Email+'</a>');
+			}
+			else $(".emaillink").html('NA');
+			if(value.contact.Website!="") {
+				$('.weblink').attr('href')="+value.contact.Website+";
+				$('.weblink').html('+value.contact.Website+')
+				//$("#storeweb").html('<a class="storeweblink" href="'+value.contact.Website+'">'+value.contact.Website+'</a>');
+			}
+			else $(".weblink").html('NA');
 		}
 	});
 	$.mobile.changePage("#details");
