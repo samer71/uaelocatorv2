63243575651e2b851cc2dd98610058a642f37252
samer71 <samer_aoudi@hotmail.com>
2013-10-21 15:35:10 +0400

    back

--- js/myApp.js
+++ js/myApp.js
@@ -279,7 +279,7 @@ $('#goback').on('tap', function ()  {
 });
 
 
-$('.onestore').on('tap', function ()  {
+$('#list').delegate('.onestore', 'tap', function ()  {
 	linkid = parseInt($(this).attr('id'));
 	$.each(sortedstore,function(index,value){ 
 		if(linkid==(index+1))
