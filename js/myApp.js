6f8f4d0af3e0412f56903b235ed902137abf3aa2
samer71 <samer_aoudi@hotmail.com>
2013-10-23 20:39:00 +0400

    fc

--- js/myApp.js
+++ js/myApp.js
@@ -384,7 +384,10 @@ $('#detailslist').delegate('.loclink', 'tap', function (event)  {
 	  };
 	directionsService.route(request, function(response, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(response);
+			google.maps.event.addListener(directionsDisplay, 'directions_changed', function () {
+    			directionsDisplay.setDirections(response);
+			});
+			
 			/*
 			var myRoute = response.routes[0].legs[0];
 			$("#dlist").append('<li class="onestep"><h3>Start</h3>'+response.routes[0].legs[0].start_address+'</li>');
@@ -401,10 +404,10 @@ $('#detailslist').delegate('.loclink', 'tap', function (event)  {
 	  });
 	  //setTimeout(function () {$("#dpanel").panel("open");}, 100); // delay above zero
 	  //$.mobile.hidePageLoadingMsg();
-	  google.maps.event.addListener(directionsDisplay, 'directions_changed', function () {
-    		alert("event dir changed");
+	 // google.maps.event.addListener(directionsDisplay, 'directions_changed', function () {
+    		//alert("event dir changed");
 			$.mobile.changePage("#directions");
-		});
+		//});
 	  //setTimeout(function () {$.mobile.changePage("#directions");}, 200); 
 	  //$.mobile.changePage("#directions");
 });
